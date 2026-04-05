482a9a235c5ac772f3360e6bc312df4073711fed
akshay12ux <kumarakshay08360@gmail.com>
2026-04-05 21:19:13 +0530

    Create index.tsx for Moodify Dashboard layout

---
+++ 3. pages/index.tsx
@@ -0,0 +1,10 @@
+import EventForm from "../components/EventForm";
+
+export default function Home() {
+return (
+<div className="p-5">
+<h1 className="text-2xl font-bold">Moodify Dashboard</h1>
+<EventForm />
+</div>
+);
+}
